faf5508761bba1289ef6ec93995c38c875772064
cesitar871-commits <cesitar871@gmail.com>
2025-12-11 21:23:19 -0600

    Add Home component for Luxora Shop

---
+++ app/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Luxora Shop</h1>
+      <p>Construyendo tu tienda premium…</p>
+    </div>
+  );
+}
